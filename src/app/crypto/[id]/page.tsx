44491592a5553a6499bcf715be262792c4e9a202
jovenan <jovenan2001@gmail.com>
2025-06-14 19:38:35 -0300

    props

--- src/app/crypto/[id]/page.tsx
+++ src/app/crypto/[id]/page.tsx
@@ -1,7 +1,14 @@
 import Details from "@/components/crypto-details/details";
 import { fetchCoinDetails } from "@/services/get-coin";
 
-export default async function CryptoPage({ params }: { params: { id: string } }) {
+type Props = {
+    params: {
+        id: string;
+    };
+    searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export default async function CryptoPage({ params }: Props) {
     const { id } = params;
     const coin = await fetchCoinDetails(id);
     return <Details initialData={coin} />;
